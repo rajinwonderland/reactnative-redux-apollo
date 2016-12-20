4c3e8c9154dd29b1f43f3b77671247adf1fba885
Jake Lacey <jakewlacey@gmail.com>
2016-12-20 17:50:09 +0000

    Adding entry component for application

---
+++ app/index.js
@@ -0,0 +1,25 @@
+/**
+ * Entry point
+ */
+
+import React, { Component } from 'react'
+import {
+  View,
+  Text
+} from 'react-native'
+
+export default class App extends Component {
+
+  render() {
+
+    return(
+      <View>
+        <Text>
+          This is to test CR.
+        </Text>
+      </View>
+    )
+
+  }
+
+}
