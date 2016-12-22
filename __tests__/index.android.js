14c65536bd24d8c7d669d4938e146fdbb5fa12ea
Jake Lacey <jakewlacey@gmail.com>
2016-12-22 21:12:28 +0000

    refactor(Components): Moving components
    Moving the single source of truth to inside app directory

--- __tests__/index.android.js
+++ __tests__/index.android.js
@@ -1,7 +1,7 @@
 import 'react-native'
 import React from 'react'
 import renderer from 'react-test-renderer'
-import Index from '../index.android'
+import Index from '../app'
 
 it('renders correctly', () => {
   renderer.create(
--- app/index.js
+++ app/index.js
@@ -1,14 +1,33 @@
 import React from 'react'
 import {
-  View,
-  Text,
+  AppRegistry,
 } from 'react-native'
+import createLogger from 'redux-logger'
+import thunkMiddleware from 'redux-thunk'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { ApolloProvider } from 'react-apollo'
 
-export default () => (
-  <View>
-    <Text>
-      This is the base component
-      another change.
-    </Text>
-  </View>
+import AppContainer from './containers/AppContainer'
+import reducer from './reducers'
+import { client } from './apollo'
+
+const loggerMiddleware = createLogger({ predicate: () => __DEV__ })
+
+function configStore(initState) {
+  const enhancer = compose(
+    applyMiddleware(thunkMiddleware, loggerMiddleware),
+  )
+  return createStore(reducer, initState, enhancer)
+}
+
+const store = configStore({})
+
+const App = () => (
+  <ApolloProvider store={store} client={client} >
+    <AppContainer />
+  </ApolloProvider>
 )
+
+AppRegistry.registerComponent('reduxApolloBoilerplate', () => App)
+
+export default App;
